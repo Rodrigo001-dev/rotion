2b2c79363bf884f727556f875562dc9c794bebde
Rodrigo <rodrigorael53@gmail.com>
2023-02-13 11:21:19 -0300

    :lipstick: style: setting the tailwind

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,30 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   renderer: {
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss({
+            config: "./tailwind.config.js",
+          }),
+        ],
+      },
+    },
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+        "@renderer": resolve("src/renderer/src"),
+      },
     },
-    plugins: [react()]
-  }
-})
+    plugins: [react()],
+  },
+});

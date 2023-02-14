e3f9775c8cfe1181f79bfa5d5d9ad062b78b9ad7
Rodrigo <rodrigorael53@gmail.com>
2023-02-14 02:31:25 -0300

    :lipstick: feat: create the skeleton with many components

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,19 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
 import "./styles/global.css";
 
 export function App() {
-  return <div className="h-screen w-screen text-rotion-100"></div>;
+  return (
+    <div className="h-screen w-screen text-rotion-100 flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+
+        <main className="flex-1 flex items-center justify-center text-rotion-400">
+          Selecione ou crie um documento
+        </main>
+      </div>
+    </div>
+  );
 }

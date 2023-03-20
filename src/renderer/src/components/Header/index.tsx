29ee7d8b6b20da56ff2a1d7027de824988dc1f87
Rodrigo <rodrigorael53@gmail.com>
2023-03-19 21:11:53 -0300

    :sparkles: feat: collapsible Sidebar

--- src/renderer/src/components/Header/index.tsx
+++ src/renderer/src/components/Header/index.tsx
@@ -1,31 +1,36 @@
-import clsx from 'clsx'
-import { Code, CaretDoubleRight, TrashSimple } from 'phosphor-react'
-import * as Breadcrumbs from './Breadcrumbs'
+import * as Collapsible from "@radix-ui/react-collapsible";
+import clsx from "clsx";
+import { Code, CaretDoubleRight, TrashSimple } from "phosphor-react";
 
-export function Header() {
-  const isMacOS = process.platform === 'darwin'
-  const isSidebarOpen = true
+import * as Breadcrumbs from "./Breadcrumbs";
+
+interface HeaderProps {
+  isSidebarOpen: boolean;
+}
+
+export function Header({ isSidebarOpen }: HeaderProps) {
+  const isMacOS = process.platform === "darwin";
 
   return (
     <div
       id="header"
       className={clsx(
-        'border-b border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag',
+        "border-b border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag",
         {
-          'pl-24': !isSidebarOpen && isMacOS,
-          'w-screen': !isSidebarOpen,
-          'w-[calc(100vw-240px)]': isSidebarOpen,
-        },
+          "pl-24": !isSidebarOpen && isMacOS,
+          "w-screen": !isSidebarOpen,
+          "w-[calc(100vw-240px)]": isSidebarOpen,
+        }
       )}
     >
-      <button
-        className={clsx('h-5 w-5 text-rotion-200 hover:text-rotion-50', {
+      <Collapsible.Trigger
+        className={clsx("h-5 w-5 text-rotion-200 hover:text-rotion-50", {
           hidden: isSidebarOpen,
           block: !isSidebarOpen,
         })}
       >
         <CaretDoubleRight className="h-4 w-4" />
-      </button>
+      </Collapsible.Trigger>
 
       <>
         <Breadcrumbs.Root>
@@ -49,5 +54,5 @@ export function Header() {
         </div>
       </>
     </div>
-  )
+  );
 }
--- src/renderer/src/pages/layouts/default.tsx
+++ src/renderer/src/pages/layouts/default.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
+import * as Collapsible from "@radix-ui/react-collapsible";
 import { Outlet } from "react-router-dom";
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
 export function Default() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <div className="h-screen w-screen text-rotion-100 flex">
+    <Collapsible.Root
+      defaultOpen
+      onOpenChange={setIsSidebarOpen}
+      className="h-screen w-screen text-rotion-100 flex"
+    >
       <Sidebar />
       <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
+        <Header isSidebarOpen={isSidebarOpen} />
 
         {/* 
             o Outlet é como se fosse o children, passando esse Outlet informamos
@@ -16,6 +24,6 @@ export function Default() {
           */}
         <Outlet />
       </div>
-    </div>
+    </Collapsible.Root>
   );
 }

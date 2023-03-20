974004626a79ed7490748913462fbe2888b7db13
Rodrigo <rodrigorael53@gmail.com>
2023-03-20 05:04:27 -0300

    :sparkles: feat: IPC handlers structure

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,13 +1,21 @@
 import { ipcMain } from "electron";
 
+import { IPC } from "@shared/constants/ipc";
+import { FetchAllDocumentsResponse } from "@shared/types/ipc";
+
 // essa estratégia de comunicação entre processos do electron, é totalmente
 // baseada em nomenclaturas para os eventos, cada evento precisa ter um titulo,
 // uma string quer represente aquele evento
-ipcMain.handle("fetch-documents", async () => {
-  return [
-    { id: "1", title: "Ignite" },
-    { id: "2", title: "Discover" },
-    { id: "3", title: "Rocketseat" },
-    { id: "4", title: "Docs" },
-  ];
-});
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH_ALL,
+  async (): Promise<FetchAllDocumentsResponse> => {
+    return {
+      data: [
+        { id: "1", title: "Ignite", content: "" },
+        { id: "2", title: "Discover", content: "" },
+        { id: "3", title: "Rocketseat", content: "" },
+        { id: "4", title: "Docs", content: "" },
+      ],
+    };
+  }
+);
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -5,6 +5,9 @@
 import { contextBridge, ipcRenderer } from "electron";
 import { electronAPI, ElectronAPI } from "@electron-toolkit/preload";
 
+import { IPC } from "@shared/constants/ipc";
+import { FetchAllDocumentsResponse } from "@shared/types/ipc";
+
 declare global {
   export interface Window {
     electron: ElectronAPI;
@@ -14,8 +17,8 @@ declare global {
 
 // Custom APIs for renderer
 const api = {
-  fetchDocuments(): Promise<Array<{ id: string; title: string }>> {
-    return ipcRenderer.invoke("fetch-documents");
+  fetchDocuments(): Promise<FetchAllDocumentsResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH_ALL);
   },
 };
 
